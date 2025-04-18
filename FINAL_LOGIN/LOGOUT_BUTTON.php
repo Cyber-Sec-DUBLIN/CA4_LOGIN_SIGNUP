558c9b1ee82ff8a80356a7a15f41dcc5dc2a7376
Darragh O'Halloran <B00162833@mytudublin.ie>
2025-04-18 13:57:20 +0100

    Delete FINAL_LOGIN directory

--- FINAL_LOGIN/LOGOUT_BUTTON.php
+++
@@ -1,5 +0,0 @@
-<?php
-//set as the action for a logout button which uses html forms similar to the lab work thing we did for web dev
-require_once 'session/session.php';
-$session = new session();
-$session->forgetSession();
